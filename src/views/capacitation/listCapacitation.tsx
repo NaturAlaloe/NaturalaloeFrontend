a1a035dc533886f7ea6d1dda9038207dff771882
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-05-30 23:38:02 -0600

    Inicio

--- src/views/capacitation/listCapacitation.tsx
+++ src/views/capacitation/listCapacitation.tsx
@@ -3,7 +3,7 @@
 function listCapacitation() {
   return (
     <div>
-        <h1 className='text-3xl font-bold underline'>Lista de Capacitaciones</h1>
+        <h1 className='text-3xl font-bold underline text-white'>Lista de Capacitaciones</h1>
         <p className='mt-4'>Aquí se mostrarán las capacitaciones disponibles.</p>
         {/* Aquí puedes agregar más contenido, como una tabla o lista de capacitaciones */}
       
