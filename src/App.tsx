d3145514e0d5b28e88d4cd1366b905ca6aca207c
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-01 23:04:52 -0600

    Add custom toast notifications using react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import Drawer from './Drawer';
+import CustomToaster from './components/CustomToaster';
 
 function App() {
   return (
+    <>
+    <CustomToaster />
     <BrowserRouter>
       <Drawer />
     </BrowserRouter>
+  </>
   );
 }
 
