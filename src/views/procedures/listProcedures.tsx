915434f5f07710d8e02c7e7580a6d1c3684ec7e4
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-11 16:29:36 -0600

    Update listProcedures.tsx

--- src/views/procedures/listProcedures.tsx
+++ src/views/procedures/listProcedures.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import DataTable from 'react-data-table-component';
 
-
 export default function ListProcedures() {
   const {
     procedures,
@@ -233,6 +232,7 @@ export default function ListProcedures() {
               disabled={currentPage === 1}
             >
               <span className="sr-only">Anterior</span>
+              <span className="sr-only">Anterior</span>
               <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
               </svg>
@@ -328,7 +328,7 @@ export default function ListProcedures() {
           sortServer
           onSort={(column, sortDirection) => {
             if (typeof column.selector === "function" && typeof column.name === "string") {
-              handleSort(column.name as keyof Procedure, sortDirection, column);
+              handleSort(column.name as keyof Procedure, sortDirection, column, searchTerm);
             }
           }}
           sortIcon={
