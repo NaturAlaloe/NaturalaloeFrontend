cc294de6abe610dfd6bd28666693e0b759234489
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-16 23:22:14 -0600

    Update proceduresList

---
+++ src/services/procedures/procedureService.ts
@@ -0,0 +1,19 @@
+import api from "../../apiConfig/api";
+
+export interface Procedure {
+  id_poe: number | null;
+  titulo: string;
+  departamento: string;
+  responsable: string;
+  revision: string;
+  fecha_vigencia: string;
+  estado: string;
+}
+
+export const getActiveProcedures = async (): Promise<Procedure[]> => {
+  const response = await api.get("/procedureActive");
+  if (response.data.success) {
+    return response.data.data;
+  }
+  throw new Error("Error al obtener los procedimientos");
+};
