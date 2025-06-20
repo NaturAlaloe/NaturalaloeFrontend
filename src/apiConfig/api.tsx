795da40164cb882fdeadc9acf5fc6509fbd89333
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-20 16:12:43 -0600

    Update api.tsx

--- src/apiConfig/api.tsx
+++ src/apiConfig/api.tsx
@@ -5,6 +5,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // <-- Esto permite enviar y recibir cookies
 });
 
 export default api;
