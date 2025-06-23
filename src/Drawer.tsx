05b5a424335c85fc91d6b05963f0457337dcfde8
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-22 23:00:56 -0600

    Update policies

--- src/Drawer.tsx
+++ src/Drawer.tsx
@@ -41,6 +41,7 @@ import ListaCategorias from "./views/administration/listCategory";
 import ListaPuestos from "./views/administration/listWorkstations";
 import SchoolIcon from "@mui/icons-material/School";
 import Politicies from "./views/politics/politicsForm";
+import PoliticiesLista from "./views/politics/politicsList";
 import VersionControlProcedures from "./views/procedures/versionControllProcedures";
 import AssignRol from "./views/collaborators/assingRol"
 
@@ -396,6 +397,22 @@ export default function PersistentDrawerLeft({ onLogout }: DrawerProps) {
                 <ListItemText primary="Agregar Políticas" />
               </ListItemButton>
             </List>
+            <List component="div" disablePadding>
+              <ListItemButton
+                component={Link}
+                to="/politics/politicsList"
+                sx={{
+                  pl: 6,
+                  color: "#f4fcec",
+                  "&:hover": {
+                    background: "#2AAC67",
+                    color: "#fff",
+                  },
+                }}
+              >
+                <ListItemText primary="Lista de Políticas" />
+              </ListItemButton>
+            </List>
           </Collapse>
 
           {/* Capacitaciones */}
@@ -611,55 +628,20 @@ export default function PersistentDrawerLeft({ onLogout }: DrawerProps) {
           <Route path="/administration/listCategory" element={<ListaCategorias />} />
           <Route path="/" element={<HomeScreen />} />
           <Route path="/procedures" element={<Procedimientos />} />
-          <Route
-            path="/procedures/listProcedures"
-            element={<ListaProcedimientos />}
-          />
-          <Route
-            path="/procedures/assignmentProcedures"
-            element={<AsignacionProcedimientos />}
-          />
+          <Route path="/procedures/listProcedures"element={<ListaProcedimientos />}/>
+          <Route path="/procedures/assignmentProcedures"element={<AsignacionProcedimientos />}/>
           <Route path="/capacitation/" element={<Capacitaciones />} />
-          <Route
-            path="/capacitation/listCapacitation"
-            element={<ListaCapacitaciones />}
-          />
-          <Route
-            path="/collaborators/collaborators"
-            element={<Colaboradores />}
-          />
-
-          <Route
-            path="/collaborators/detail/:id"
-            element={<CollaboratorDetail />}
-          />
-
-          <Route
-            path="/capacitation/facilitatorTraining"
-            element={<RegistroFacilitadores />}
-          />
-
-          <Route
-            path="/capacitation/listFacilitators"
-            element={<ListaFacilitadores />}
-          />
-          <Route
-            path="/capacitation/capacitationFinished"
-            element={<CapacitacionFinalizada />}
-          />
-          <Route
-            path="/collaborators/addCollaborator"
-            element={<AddCollaborator />}
-          />
+          <Route path="/capacitation/listCapacitation"element={<ListaCapacitaciones />} />
+          <Route path="/collaborators/collaborators" element={<Colaboradores />} />
+          <Route path="/collaborators/detail/:id" element={<CollaboratorDetail />} />
+          <Route path="/capacitation/facilitatorTraining" element={<RegistroFacilitadores />} />
+          <Route path="/capacitation/listFacilitators" element={<ListaFacilitadores />} />
+          <Route path="/capacitation/capacitationFinished" element={<CapacitacionFinalizada />} />
+          <Route path="/collaborators/addCollaborator" element={<AddCollaborator />} />
           <Route path="/politics/politicsForm" element={<Politicies />} />
-          <Route
-            path="/procedures/versionControlProcedures"
-            element={<VersionControlProcedures />}
-          />
-          <Route
-          path="/collaborators/assingRol"
-          element={<AssignRol/>}
-        />
+          <Route path="/politics/politicsList" element={<PoliticiesLista />} />
+          <Route path="/procedures/versionControlProcedures" element={<VersionControlProcedures />} />
+          <Route path="/collaborators/assingRol" element={<AssignRol/>} />
         </Routes>
       </Main>
     </Box>
