7c76e5112ee04e230f6f2ee30807b5e12d9e1b7a
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-06-28 18:29:28 -0600

    Update useUserFromToken.ts

--- src/hooks/useUserFromToken.ts
+++ src/hooks/useUserFromToken.ts
@@ -1,8 +1,13 @@
 import { useMemo } from "react";
 
+function getCookie(name: string) {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? decodeURIComponent(match[2]) : null;
+}
+
 export function useUserFromToken() {
   return useMemo(() => {
-    const token = localStorage.getItem("token");
+    const token = getCookie("token");
     if (!token) return null;
     try {
       const payload = JSON.parse(atob(token.split(".")[1]));
