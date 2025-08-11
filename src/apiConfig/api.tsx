5c8f49e927de539cc912043223de56e9cd03d419
MariaJesus12 <marychuartaviasol2606@gmail.com>
2025-08-11 12:40:31 -0600

    Update api.tsx

--- src/apiConfig/api.tsx
+++ src/apiConfig/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://naturalaloefrontend-production.up.railway.app/api', 
+  baseURL: 'https://naturalaloe.dev/api-docs/api', 
   headers: {
     'Content-Type': 'application/json',
   },
