9a0e294e9b49105fe7ecb87bb25ddf2e31a2d3c8
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-08-11 19:06:27 -0600

    Update api.tsx

--- src/apiConfig/api.tsx
+++ src/apiConfig/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://naturalaloebackend-production.up.railway.app/api',
+  baseURL: 'https://naturalaloe.dev/api',
   headers: {
     'Content-Type': 'application/json',
   },
