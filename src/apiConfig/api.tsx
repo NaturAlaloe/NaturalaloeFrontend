aec3957f54680ab79a5618edc442fe574e622369
Alessandro Piña Pérez <carodri323@gmail.com>
2025-08-16 11:15:19 -0600

    Update API baseURL to local server address
    Changed the axios baseURL from the production endpoint to a local server address for development or testing purposes.

--- src/apiConfig/api.tsx
+++ src/apiConfig/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://naturalaloe.dev/api',
+  baseURL: 'http://10.1.0.4:4000/api',
   headers: {
     'Content-Type': 'application/json',
   },
