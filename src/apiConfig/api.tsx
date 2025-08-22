60206700523e2165f2259c1e8ad72ab549161af5
EkarCortes <141070013+EkarCortes@users.noreply.github.com>
2025-08-21 23:40:48 -0600

    Update api.tsx

--- src/apiConfig/api.tsx
+++ src/apiConfig/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.205:4000/api',
+  baseURL: 'https://naturalaloe.dev/api',
   headers: {
     'Content-Type': 'application/json',
   },
